4645b33532000bda0fef23fc99def9212560bc32
Ignacio Armella <gabrielarmella.70@gmail.com>
2025-05-09 23:14:18 -0300

    Confirmo todo el proyecto para enviarlo

---
+++ src/com/aluracursos/mymoneyconverter/modelos/Moneda.java
@@ -0,0 +1,9 @@
+package com.aluracursos.mymoneyconverter.modelos;
+
+public record Moneda(
+        String result,
+        String base_code,
+        String target_code,
+        double conversion_rate,
+        double conversion_result
+) {}
